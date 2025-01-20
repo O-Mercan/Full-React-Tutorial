793b247e1b548685e1762743523e0b60c974d1c1
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-20 17:13:42 +0300

    added lecture 21: The React Router

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,13 +1,21 @@
 import Navbar from './Navbar';
 import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content"></div>
-      <Home></Home>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="content">
+          <Switch>
+            <Route path = "/">
+              <Home></Home>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
