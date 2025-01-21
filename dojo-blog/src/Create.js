7652669c897fcc4e21b5e33de0399d32a553a255
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-21 14:42:13 +0300

    added lecture 28: Submit Events

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -5,11 +5,18 @@ const [title, setTitle] = useState('');
 const [body, setBody] = useState('');
 const [author, setAuthor] = useState('mario');
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog)
+}
+
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-                <form >
+                <form onSubmit={handleSubmit}>
                     <label >Blog Title:</label>
                     <input 
                         type="text"
@@ -32,9 +39,6 @@ const [author, setAuthor] = useState('mario');
                         <option value="yoshi">yoshi</option>
                     </select>
                     <button>Add Blog</button>
-                    <p>{ title }</p>
-                    <p>{ body }</p>
-                    <p>{ author }</p>
                 </form>
         </div>
       );
